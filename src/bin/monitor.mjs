a9d477b053e087fabe993d6a5d5aeed565d3515d
Edwin Pers <pers.edwin@gmail.com>
2025-09-06 14:31:24 -0400

    fixup monitor.mjs

--- src/bin/monitor.mjs
+++ src/bin/monitor.mjs
@@ -5,27 +5,20 @@
 import * as Nats from "nats";
 
 const natsClients = [];
+const subscriptions = [];
 
 //
 // Do whatever teardown is necessary before calling common handler
 process.on("SIGINT", () => {
-  ircClients.forEach((ircClient) => {
-    ircClient.quit(`SIGINT received - ${ircClient.ident.quitMsg}`);
-  });
   natsClients.forEach((natsClient) => {
     natsClient.drain();
   });
-  handleSIG("SIGINT");
 });
 
 process.on("SIGTERM", () => {
-  ircClients.forEach((ircClient) => {
-    ircClient.quit(`SIGTERM received - ${ircClient.ident.quitMsg}`);
-  });
   natsClients.forEach((natsClient) => {
     natsClient.drain();
   });
-  handleSIG("SIGTERM");
 });
 
 //
@@ -35,14 +28,12 @@ process.on("SIGTERM", () => {
 const natsHost = process.env.NATS_HOST || false;
 if (!natsHost) {
   const msg = "environment variable NATS_HOST is not set.";
-  log.error(msg, { producer: "natsClient" });
   throw new Error(msg);
 }
 
 const natsToken = process.env.NATS_TOKEN || false;
 if (!natsToken) {
   const msg = "environment variable NATS_TOKEN is not set.";
-  log.error(msg, { producer: "natsClient" });
   throw new Error(msg);
 }
 
@@ -57,10 +48,7 @@ const sub = nats.subscribe('>');
 subscriptions.push(sub);
 (async () => {
   for await (const message of sub) {
-    log.info(`[${message.subject}][${sub.getProcessed()}]: ${message.string()}`, { producer: "natsClient" });
-    if (typeof callback == "function") {
-      callback(subject, message);
-    }
+    // eslint-disable-next-line no-console
+    console.log(`[${message.subject}][${sub.getProcessed()}]: ${message.string()}`, { producer: "natsClient" });
   }
-  log.info("subscription closed", { producer: "natsClient" });
 })();
